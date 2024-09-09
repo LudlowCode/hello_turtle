0e89f3ec1c9e01a33d053e4bfc08063d04c178fe
dedwards@thequeensschool.co.uk <dedwards@thequeensschool.co.uk>
2024-09-09 14:17:54 +0100

    added broken triangle

--- hello.py
+++ hello.py
@@ -3,6 +3,8 @@ import turtle
 
 turtle.fillcolor("orange")
 turtle.pencolor("blue")
+
+#draw a square
 sides = 4
 i = 0
 turtle.begin_fill()
@@ -11,4 +13,16 @@ while i < sides:
     turtle.right(90)
     i = i + 1
 turtle.end_fill()
+
+#draw a triangle
+turtle.goto(150,150)
+sides = 3
+i = 0
+turtle.begin_fill()
+while i < sides:
+    turtle.forward(100)
+    turtle.right(120)
+    i = i + 1
+turtle.end_fill()
+
 input()
